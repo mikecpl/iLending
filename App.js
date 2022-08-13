5b6d76756c0899e048d2c2b69ef50dc969e34cff
mikecpl <mikecorporal@gmail.com>
2022-08-14 00:55:22 +0200

    Added base login page.

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
 import { LinearGradient } from 'expo-linear-gradient';
+import { SafeAreaView, Text, View } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
+    <View className="flex-1">
+      <StatusBar style="light" />
       <LinearGradient
-        colors={['rgba(0,0,0,0.8)', 'transparent']}
+        colors={['#FF826F', '#FC406F', 'F70039']}
         className="h-full w-full absolute"
       />
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView className="flex-1 flex-col items-center justify-evenly">
+        <View className="flex flex-col items-center">
+          <FontAwesome5
+            name="hand-holding-usd"
+            size={48}
+            color="white"
+          />
+          <Text className="text-white text-3xl mt-8">iLending</Text>
+          <Text className="text-gray-200 text-sm">Handle your debts and lendings easily</Text>
+        </View>
+        <View>
+          <Text className="text-white text-3xl mt-8">iLending</Text>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
