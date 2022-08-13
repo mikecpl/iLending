39f39e01ba8c16e92e3c0bca1345bf825c48d26a
mikecpl <mikecorporal@gmail.com>
2022-08-14 00:23:26 +0200

    Added linear gradient.

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center">
+      <LinearGradient
+        colors={['rgba(0,0,0,0.8)', 'transparent']}
+        className="h-full w-full absolute"
+      />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
