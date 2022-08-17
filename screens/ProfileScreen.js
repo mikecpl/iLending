e826993ddd487dffe39e1800d36e28881a396aa5
mikecpl <mikecorporal@gmail.com>
2022-08-17 23:20:10 +0200

    Added logout to profile.

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
+import useAuth from '../hooks/useAuth';
 
 const ProfileScreen = () => {
+  const { logout } = useAuth();
+
   return (
     <View>
       <Text>ProfileScreen</Text>
+      <TouchableOpacity onPress={logout}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
     </View>
   )
 }
