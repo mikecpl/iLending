996eddf3836edb9111d92594a958714bf644b914
mikecpl <mikecorporal@gmail.com>
2022-08-17 23:20:47 +0200

    Added safeareaview.

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, SafeAreaView } from 'react-native';
 import React from 'react';
 import useAuth from '../hooks/useAuth';
 
@@ -6,12 +6,12 @@ const ProfileScreen = () => {
   const { logout } = useAuth();
 
   return (
-    <View>
+    <SafeAreaView>
       <Text>ProfileScreen</Text>
       <TouchableOpacity onPress={logout}>
         <Text>Logout</Text>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   )
 }
 
