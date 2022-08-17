33f1ba799e735899b744c7c8355411a98861132c
mikecpl <mikecorporal@gmail.com>
2022-08-17 22:32:42 +0200

    Added screens.

---
+++ screens/DebtScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const DebtScreen = () => {
+  return (
+    <View>
+      <Text>DebtScreen</Text>
+    </View>
+  )
+}
+
+export default DebtScreen;
