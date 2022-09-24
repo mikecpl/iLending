d3eef33b1a6d42ef125ebb17c35c1dc4039d7a1d
mikecpl <mikecorporal@gmail.com>
2022-09-24 17:22:01 +0200

    Added loader.

---
+++ components/app/Loader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ActivityIndicator } from 'react-native';
+
+const Loader = () => {
+  return <ActivityIndicator className="flex-1" color="#BE123C" />;
+}
+
+export default Loader;
