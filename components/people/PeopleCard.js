487b07ae9c37bf6422dfcc3181d92a4ce93b383d
mikecpl <mikecorporal@gmail.com>
2022-10-12 21:53:08 +0200

    CS fix.

--- components/people/PeopleCard.js
+++ components/people/PeopleCard.js
@@ -2,7 +2,7 @@ import { View, TouchableOpacity } from 'react-native';
 import React from 'react';
 import CustomText from '../app/CustomText';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { faArrowDown, faUser } from '@fortawesome/free-solid-svg-icons';
 import colors from 'tailwindcss/colors';
 import { useNavigation } from '@react-navigation/native';
 
@@ -13,18 +13,21 @@ const PeopleCard = () => {
     <TouchableOpacity className="h-14 flex flex-row justify-between items-center space-x-2 mb-2"
       onPress={() => navigation.navigate('PaymentModal')}
     >
-      <View className="bg-red-500 justify-center p-2 rounded-lg">
-        <FontAwesomeIcon icon={faArrowDown} color={colors.white} size={18} />
+      <View className="bg-slate-700 justify-center p-4 rounded-full">
+        <FontAwesomeIcon icon={faUser} color={colors.white} size={18} />
       </View>
       <View className="flex flex-col grow justify-between pl-2 pr-4">
         <CustomText className="w-32 text-white text-base">
           Somogyi Gergő
         </CustomText>
         <CustomText className="text-slate-500">
-          SPAR bevásárlás
+          3 payments
+        </CustomText>
+        <CustomText className="text-slate-500">
+          2020 October 10
         </CustomText>
       </View>
-      <View className="flex flex-row items-center justify-end">
+      <View>
         <CustomText className="text-red-500 mr-2">
           - $300
         </CustomText>
