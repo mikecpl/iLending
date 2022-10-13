a1569548956315d1549dcdb61cacf22234c73739
mikecpl <mikecorporal@gmail.com>
2022-10-13 22:09:38 +0200

    Removed the fortawesome package.

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,4 @@
 import { Platform, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faHandHoldingDollar } from '@fortawesome/free-solid-svg-icons/faHandHoldingDollar';
-import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 import * as AppleAuthentication from 'expo-apple-authentication';
 import useAuth from '../hooks/useAuth';
 import { useEffect } from 'react';
@@ -11,6 +8,9 @@ import * as Google from 'expo-auth-session/providers/google';
 import Constants from 'expo-constants';
 import CustomText from '../components/app/CustomText';
 import Background from '../components/app/Background';
+import Svg, { Path } from 'react-native-svg';
+import { BanknotesIcon } from 'react-native-heroicons/outline';
+import colors from 'tailwindcss/colors';
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -32,11 +32,7 @@ const LoginScreen = () => {
       <Background />
       <SafeAreaView className="flex-1 flex-col items-center justify-evenly">
         <View className="flex flex-col items-center">
-          <FontAwesomeIcon
-            icon={faHandHoldingDollar}
-            size={48}
-            color="white"
-          />
+          <BanknotesIcon size={54} color={colors.white} />
           <CustomText className="text-white text-4xl mt-8">
             iLending
           </CustomText>
@@ -53,12 +49,12 @@ const LoginScreen = () => {
               })
             }}
           >
-            <FontAwesomeIcon
-              icon={faGoogle}
-              size={12}
-              color="black"
-            />
-            <Text className="text-base font-semibold">Sign in with Google</Text>
+            <Svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="black" viewBox="0 0 16 16">
+              <Path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/>
+            </Svg>
+            <Text className="text-base font-semibold">
+              Sign in with Google
+            </Text>
           </TouchableOpacity>
 
           {Platform.OS === 'ios' &&
