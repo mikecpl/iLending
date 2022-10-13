d1681ba3954c2e2a5fab432055d3f4cb27b466cb
mikecpl <mikecorporal@gmail.com>
2022-10-13 22:09:52 +0200

    Removed the custom colors.

--- components/app/Loader.js
+++ components/app/Loader.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { ActivityIndicator } from 'react-native';
+import colors from '../../etc/colors';
 
 const Loader = () => {
-  return <ActivityIndicator className="flex-1" color="#BE123C" />;
+  return <ActivityIndicator className="flex-1" color={colors['ilending-sky'][600]} />;
 }
 
 export default Loader;
--- etc/colors.js
+++ etc/colors.js
@@ -1,8 +1,4 @@
 module.exports = {
-  'ilending': {
-    800: '#352e68',
-    900: '#29205f'
-  },
   'ilending-sky': {
     600: '#226fbe'
   }
