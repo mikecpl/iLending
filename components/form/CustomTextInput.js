30fbc7eb6761f946a6876f21cf92e82a5b8e3a86
mikecpl <mikecorporal@gmail.com>
2022-10-18 22:01:26 +0200

    Added custom text input.

---
+++ components/form/CustomTextInput.js
@@ -0,0 +1,65 @@
+import { View, TextInput } from 'react-native';
+import React from 'react';
+import colors from 'tailwindcss/colors';
+import { MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+import CustomText from '../app/CustomText';
+
+const CustomTextInput = ({ title, icon, placeholder, helperText, errors = [] }) => {
+  const hasError = errors.length > 0;
+  let rowClassNames = 'flex flex-row items-center space-x-1 bg-slate-700 px-2 py-1 rounded-lg';
+  let titleClassNames = 'text-slate-400';
+
+  if (hasError) {
+    rowClassNames += ' border border-red-500';
+    titleClassNames = 'text-red-500';
+  }
+
+  return (
+    <View>
+      {placeholder && (
+        <View className="mb-1">
+          <CustomText className={titleClassNames}>
+            {title}
+          </CustomText>
+        </View>
+      )}
+      <View className={rowClassNames}>
+        {icon && (
+          <View className="pl-1">
+            <MagnifyingGlassIcon color={colors.slate[400]} size={20} />
+          </View>
+        )}
+
+        <View className="px-1">
+          <TextInput className="text-white h-10 text-lg pt-1 pb-[10px] pr-6"
+            multiline={true}
+            blurOnSubmit={true}
+            numberOfLines={1}
+            placeholder={placeholder}
+            placeholderTextColor={colors.slate[400]}
+          />
+        </View>
+      </View>
+
+      { helperText && (
+        <View className="mt-2">
+            <CustomText className="text-slate-400">
+              {helperText}
+            </CustomText>
+        </View>
+      )}
+
+      { hasError && (
+        <View className="mt-2">
+          {errors.map(error => (
+            <CustomText className="text-red-500">
+              {error}
+            </CustomText>
+          ))}
+        </View>
+      )}
+    </View>
+  )
+}
+
+export default CustomTextInput;
