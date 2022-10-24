7faebb7d4c41b527600e1a8939e98849d80d4d99
mikecpl <mikecorporal@gmail.com>
2022-10-24 22:37:18 +0200

    Added payment statuses.

--- constants/payment.js
+++ constants/payment.js
@@ -1,2 +1,6 @@
 export const TYPE_DEBT = 'debt';
 export const TYPE_LOAN = 'loan';
+
+export const STATUS_PENDING = 'pending';
+export const STATUS_COMPLETED = 'completed';
+export const STATUS_DENIED = 'denied';
