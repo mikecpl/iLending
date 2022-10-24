a9f9576edb5dcc0f4c912d4a6e62958fc260e899
mikecpl <mikecorporal@gmail.com>
2022-10-24 22:37:30 +0200

    Enhanced the date picker.

--- components/form/CustomDatepicker.js
+++ components/form/CustomDatepicker.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import CustomText from '../app/CustomText';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 
-const CustomDatepicker = ({ value = new Date(), mode = 'date', title, icon, helperText, errors = [], onChange }) => {
+const CustomDatepicker = ({ value, mode = 'date', title, icon, helperText, errors = [], onChange }) => {
   const [isVisible, setIsVisible] = useState(false);
   const hasError = errors.length > 0;
   let rowClassNames = 'flex flex-row items-center space-x-1 bg-slate-700 px-2 py-3 rounded-lg';
@@ -32,9 +32,15 @@ const CustomDatepicker = ({ value = new Date(), mode = 'date', title, icon, help
           </View>
         )}
         <View className="px-1 pt-1 grow text-left">
-          <CustomText className="text-white text-base">
-            {value.toLocaleDateString()}
-          </CustomText>
+          {value ? (
+            <CustomText className="text-white text-base">
+              {value.toLocaleDateString()}
+            </CustomText>
+          ) : (
+            <CustomText className="text-slate-400 text-lg">
+              Enter a date
+            </CustomText>
+          )}
         </View>
       </TouchableOpacity>
 
