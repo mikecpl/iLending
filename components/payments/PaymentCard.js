cb9e69fd7932b0f3c94f75edca72a1730559b551
mikecpl <mikecorporal@gmail.com>
2022-11-05 11:06:20 +0100

    Fixed the payment card layout.

--- components/payments/PaymentCard.js
+++ components/payments/PaymentCard.js
@@ -7,6 +7,7 @@ import { useNavigation } from '@react-navigation/native';
 import { TYPE_DEBT } from '../../constants/payment';
 import { doc, getDoc } from 'firebase/firestore';
 import { db, transformData } from '../../firebase';
+import { errorToast } from '../app/toast';
 
 const PaymentCard = ({ payment }) => {
   const navigation = useNavigation();
@@ -17,9 +18,8 @@ const PaymentCard = ({ payment }) => {
 
     getDoc(docRef).then(snapshot => {
       setUser(transformData(snapshot));
-    }).catch(e => console.log(e));
+    }).catch(() => errorToast());
   }, []);
-  
   
   // TODO remove
   if (!payment) {
@@ -33,7 +33,7 @@ const PaymentCard = ({ payment }) => {
   const amount = `${payment.amount}Ft`;
 
   return (
-    <TouchableOpacity className="h-14 flex flex-row justify-between items-center space-x-2 mb-2"
+    <TouchableOpacity className="min-h-[48px] h-fit flex flex-row justify-between items-center space-x-2 mb-2"
       onPress={() => navigation.navigate('PaymentNavigation', {screen: 'Payment', initial: false})}
     >
       {payment.type === TYPE_DEBT
@@ -48,9 +48,9 @@ const PaymentCard = ({ payment }) => {
           </View>
         )
       }
-      <View className="flex flex-col grow justify-between pl-2 pr-4">
-        <CustomText className="w-32 text-white text-base">
-          {user.name}
+      <View className="flex flex-col w-1/3 grow shrink-0 justify-between pl-2 pr-4">
+        <CustomText className="text-white text-base">
+          Gergo Somogyi
         </CustomText>
         <CustomText className="text-slate-500">
           {payment.item}
